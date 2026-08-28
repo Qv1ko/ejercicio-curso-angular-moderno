83da1f773da1c98e95fe709cc89853691f5aadf0
Qv1ko <vicgarmur947@gmail.com>
2026-08-28 23:56:23 +0200

    feat: add null const

--- src/app/shared/domain/cart-item.type.ts
+++ src/app/shared/domain/cart-item.type.ts
@@ -1,6 +1,11 @@
-import { Product } from './product.type';
+import { NULL_PRODUCT, Product } from './product.type';
 
 export type CartItem = {
-  id: number | string;
+  id: string;
   producto: Product;
+};
+
+export const NULL_CART_ITEM: CartItem = {
+  id: '',
+  producto: NULL_PRODUCT,
 };
