2d10ae89b8ea7ce6f070ea3b87d6b39d54c380e2
Qv1ko <vicgarmur947@gmail.com>
2026-08-28 21:27:30 +0200

    feat: add product domain types

---
+++ src/app/shared/domain/product.type.ts
@@ -0,0 +1,17 @@
+export type Product = {
+    id: string,
+    nombre: string,
+    descripcion: string
+    categoria_id: number
+    precio: number
+    stock: number
+}
+
+export const NULL_PRODUCT: Product = {
+    id: '',
+    nombre: "",
+    descripcion: '',
+    categoria_id: 0,
+    precio: 0,
+    stock: 0
+}
